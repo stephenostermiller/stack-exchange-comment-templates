e8dd5c6a40552ef901c21881549b0b5f5b176aec
Stephen Ostermiller <stephenostermiller@users.noreply.github.com>
2022-05-28 16:00:01 -0400

    Trigger on /admin path for flag handling

--- context-menu.js
+++ context-menu.js
@@ -1,22 +1,29 @@
 // ==UserScript==
 // @name Stack Exchange comment template context menu
 // @namespace http://ostermiller.org/
-// @version 1.13
+// @version 1.14
 // @description Adds a context menu (right click, long press, command click, etc) to comment boxes on Stack Exchange with customizable pre-written responses.
 // @match https://*.stackexchange.com/questions/*
 // @match https://*.stackexchange.com/review/*
+// @match https://*.stackexchange.com/admin/*
 // @match https://*.stackoverflow.com/*questions/*
 // @match https://*.stackoverflow.com/review/*
+// @match https://*.stackoverflow.com/admin/*
 // @match https://*.askubuntu.com/questions/*
 // @match https://*.askubuntu.com/review/*
+// @match https://*.askubuntu.com/admin/*
 // @match https://*.superuser.com/questions/*
 // @match https://*.superuser.com/review/*
+// @match https://*.superuser.com/admin/*
 // @match https://*.serverfault.com/questions/*
 // @match https://*.serverfault.com/review/*
+// @match https://*.serverfault.com/admin/*
 // @match https://*.mathoverflow.net/questions/*
 // @match https://*.mathoverflow.net/review/*
+// @match https://*.mathoverflow.net/admin/*
 // @match https://*.stackapps.com/questions/*
 // @match https://*.stackapps.com/review/*
+// @match https://*.stackapps.com/admin/*
 // @connect raw.githubusercontent.com
 // @connect *
 // @grant GM_addStyle
